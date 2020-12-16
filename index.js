22045d16324c2b7d5b62b0b936415b1139831258
koidzt <koi27792@gmail.com>
2020-12-16 19:03:30 +0700

    revised passport for change password

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 require('dotenv').config();
+require('./middleware/passport');
+
+const db = require('./models');
+const cors = require('cors');
 const express = require('express');
 const app = express();
+
+const userRoute = require('./routes/user');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/user', userRoute);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
@@ -19,6 +31,11 @@ app.listen(process.env.PORT, () => {
   console.log(`Example app listening at http://localhost:${process.env.PORT}`);
 });
 
-// db.sequelize.sync({ force: false }).then(() => {
-//   console.log('Completed Connect And Sync');
-// });
+db.sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log('Completed Connect And Sync');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
